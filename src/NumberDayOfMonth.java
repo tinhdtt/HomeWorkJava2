bc6868486ec4165fbcc064292e6f8d01202d3326
tinhdtt <tinhdtt@gmail.com>
2021-05-12 12:28:24 +0700

    update homework 2 - Dinh Thi Tinh

--- src/NumberDayOfMonth.java
+++ src/NumberDayOfMonth.java
@@ -10,40 +10,22 @@ public class NumberDayOfMonth {
 
         switch (month){
             case 1:
-                System.out.println("There are 31 days in January");
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("There are 31 days in month " +month);
                 break;
             case 2:
                 System.out.println("There are 28 or 29 days in February");
                 break;
-            case 3:
-                System.out.println("There are 31 days in March");
-                break;
             case 4:
-                System.out.println("There are 30 days in April");
-                break;
-            case 5:
-                System.out.println("There are 31 days in May");
-                break;
             case 6:
-                System.out.println("There are 30 days in June");
-                break;
-            case 7:
-                System.out.println("There are 31 days in July");
-                break;
-            case 8:
-                System.out.println("There are 31 days in August");
-                break;
             case 9:
-                System.out.println("There are 30 days in September");
-                break;
-            case 10:
-                System.out.println("There are 31 days in October");
-                break;
             case 11:
-                System.out.println("There are 30 days in November");
-                break;
-            case 12:
-                System.out.println("There are 31 days in December");
+                System.out.println("There are 30 days in month " +month);
                 break;
             default:
                 System.out.println("Please input month from 1 to 12");
--- src/NumberDayOfMonthUseIf.java
+++ src/NumberDayOfMonthUseIf.java
@@ -6,40 +6,14 @@ public class NumberDayOfMonthUseIf {
         Scanner sc = new Scanner(System.in);
         System.out.print("Month = ");
         month = sc.nextInt();
-        if (month == 1) {
-            System.out.println("There are 31 days in January");
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month ==12) {
+            System.out.println("There are 31 days in month " +month);
         }
         else if (month == 2) {
             System.out.println("There are 28 or 29 days in February");
         }
-        else if (month == 3) {
-            System.out.println("There are 31 days in March");
-        }
-        else if (month == 4) {
-            System.out.println("There are 30 days in April");
-        }
-        else if (month == 5) {
-            System.out.println("There are 31 days in May");
-        }
-        else if (month == 6) {
-            System.out.println("There are 30 days in June");
-        }
-        else if (month == 7) {
-            System.out.println("There are 31 days in July");
-        }
-        else if (month == 8) {
-            System.out.println("There are 31 days in August");
-        }
-        else if (month == 9) {
-            System.out.println("There are 30 days in September");
-        }
-        else if (month == 10) {
-            System.out.println("There are 31 days in October");
-        }
-        else if (month == 11) {
-            System.out.println("There are 31 days in December");
-        } else if (month == 12) {
-            System.out.println("There are 31 days in December");
+        else if (month == 3 || month == 5 || month == 9 || month == 11) {
+            System.out.println("There are 30 days in month " +month);
         }
         else {
             System.out.println("Please input month from 1 to 12");
